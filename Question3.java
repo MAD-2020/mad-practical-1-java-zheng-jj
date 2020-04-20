b724abbedb84e2cf397855dc2c4abcecaac5d90d
zheng-jj <64000939+zheng-jj@users.noreply.github.com>
2020-04-20 16:46:27 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -11,6 +11,9 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    
+    System.out.print("Enter number: ");
+    Integer square = in.nextInt();
+    in.close();
+    System.out.println(String.valueOf(square)+" times "+ String.valueOf(square) +" = "+ String.valueOf(square*square));
   }
 }
